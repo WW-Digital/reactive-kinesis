f3a6689d2fea6300a85bc167d3d36dcdbd37a6d7
Matthias Veit <matthias_veit@yahoo.de>
2017-12-11 16:46:12 +0100

    Create KCL Lease tables proactively, since KCL handles this in an unreliable way.

--- src/it/scala/com/weightwatchers/reactive/kinesis/stream/KinesisSourceSpec.scala
+++ src/it/scala/com/weightwatchers/reactive/kinesis/stream/KinesisSourceSpec.scala
@@ -1,9 +1,6 @@
 package com.weightwatchers.reactive.kinesis.stream
 
-import java.io.File
-
 import akka.stream.scaladsl.Sink
-import com.typesafe.config.ConfigFactory
 import com.weightwatchers.reactive.kinesis.common.{
   AkkaUnitTestLike,
   KinesisConfiguration,
@@ -21,9 +18,7 @@ class KinesisSourceSpec
     with AkkaUnitTestLike
     with Matchers {
 
-  val TestStreamName = "int-test-stream-3"
-
-  trait WithKinesis {
+  class WithKinesis(val appName: String) {
     val workerIdGen: Iterator[String] = 1.to(Int.MaxValue).iterator.map(id => s"wrk-$id")
     def consumerConf(appName: String, batchSize: Long): ConsumerConf = {
       ConsumerConf(kinesisConfig(streamName = TestStreamName,
@@ -32,14 +27,17 @@ class KinesisSourceSpec
                                  maxRecords = batchSize.toInt),
                    "testConsumer")
     }
+
+    // proactively create the lease table for this application.
+    // KCL does not handle this reliably, which makes the test brittle.
+    createLeaseTable(appName)
   }
 
   override implicit def patienceConfig: PatienceConfig = PatienceConfig(60.seconds)
 
   "A Kinesis Source" should {
 
-    "process all messages of a stream with one worker" in new WithKinesis {
-      val appName = "1worker"
+    "process all messages of a stream with one worker" in new WithKinesis("1worker") {
       val result = Kinesis
         .source(consumerConf = consumerConf(appName, TestStreamNrOfMessagesPerShard))
         .take(TestStreamNumberOfShards * TestStreamNrOfMessagesPerShard)
@@ -55,11 +53,10 @@ class KinesisSourceSpec
       grouped.values.foreach(_ should have size TestStreamNumberOfShards)
     }
 
-    "process all messages of a stream with 2 workers" in new WithKinesis {
+    "process all messages of a stream with 2 workers" in new WithKinesis("2worker") {
       // Please note: since source1 and source2 are started simultaneously, both will assume there is no other worker.
       // During register one will fail and not read any message until retry
       // Depending on timing one or both sources will read all events
-      val appName   = "2worker"
       val batchSize = TestStreamNrOfMessagesPerShard
       val source1   = Kinesis.source(consumerConf = consumerConf(appName, batchSize))
       val source2   = Kinesis.source(consumerConf = consumerConf(appName, batchSize))
@@ -78,12 +75,11 @@ class KinesisSourceSpec
       grouped.values.foreach(_ should have size TestStreamNumberOfShards)
     }
 
-    "process all messages of a stream with 4 workers" in new WithKinesis {
+    "process all messages of a stream with 4 workers" in new WithKinesis("4worker") {
       // Please note: since all sources are started simultaneously, all will assume there is no other worker.
       // During register all except one will fail and not read any message until retry
       // Depending on timing one or multiple sources will read all events
       val batchSize = TestStreamNrOfMessagesPerShard
-      val appName   = "4worker"
       val source1   = Kinesis.source(consumerConf = consumerConf(appName, batchSize))
       val source2   = Kinesis.source(consumerConf = consumerConf(appName, batchSize))
       val source3   = Kinesis.source(consumerConf = consumerConf(appName, batchSize))
@@ -106,9 +102,8 @@ class KinesisSourceSpec
       grouped.values.foreach(_ should have size TestStreamNumberOfShards)
     }
 
-    "maintain the read position in the stream correctly" in new WithKinesis {
+    "maintain the read position in the stream correctly" in new WithKinesis("read_position") {
       val batchSize = TestStreamNrOfMessagesPerShard / 2 // 2 * NrOfShards batches needed
-      val appName   = "stream_read_position"
 
       // We create multiple Sources (one after the other!). Each source:
       // - takes batchSize of messages and commits all of them
@@ -136,9 +131,8 @@ class KinesisSourceSpec
       grouped should have size TestStreamNrOfMessagesPerShard
     }
 
-    "not commit the position, if the event is not committed" in new WithKinesis {
+    "not commit the position, if the event is not committed" in new WithKinesis("not_committed") {
       val batchSize = TestStreamNrOfMessagesPerShard / 2 // 2 * NrOfShards batches needed
-      val appName   = "stream_not_commited"
 
       // This worker will read batchSize events and will not commit
       // We expect that the read position will not change
