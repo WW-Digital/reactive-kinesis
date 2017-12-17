21034fdbee4d8baddea00a8c3f55258ee9ede7a9
Mark Harrison <markglh@gmail.com>
2017-12-17 18:34:15 +0000

    removed custom workerid from tests

--- src/it/scala/com/weightwatchers/reactive/kinesis/common/KinesisSuite.scala
+++ src/it/scala/com/weightwatchers/reactive/kinesis/common/KinesisSuite.scala
@@ -5,16 +5,13 @@ import java.nio.ByteBuffer
 
 import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration
 import com.amazonaws.services.dynamodbv2.{AmazonDynamoDB, AmazonDynamoDBClientBuilder}
-import com.amazonaws.services.kinesis.leases.impl.{
-  KinesisClientLease,
-  KinesisClientLeaseSerializer,
-  LeaseManager
-}
+import com.amazonaws.services.kinesis.leases.impl.{KinesisClientLease, KinesisClientLeaseSerializer, LeaseManager}
 import com.amazonaws.services.kinesis.model.PutRecordRequest
 import com.amazonaws.services.kinesis.{AmazonKinesisAsync, AmazonKinesisAsyncClientBuilder}
 import com.typesafe.config.{Config, ConfigFactory}
 import com.typesafe.scalalogging.StrictLogging
 import com.weightwatchers.reactive.kinesis.consumer.KinesisConsumer.ConsumerConf
+import org.scalactic.source.Position
 import org.scalatest.{BeforeAndAfter, BeforeAndAfterAll, Suite}
 
 import scala.collection.JavaConverters._
@@ -34,51 +31,66 @@ trait KinesisConfiguration {
     ConfigFactory
       .parseString(
         s"""
-         |kinesis {
-         |
-         |   application-name = "$appName"
-         |
-         |   # The name of the consumer, we can have many consumers per application
-         |   testConsumer {
-         |      # The name of the consumer stream, MUST be specified per consumer
-         |      stream-name = "$streamName"
-         |
-         |      # Use localstack for integration test
-         |      kcl {
-         |         kinesisEndpoint = "https://localhost:4568"
-         |         dynamoDBEndpoint = "https://localhost:4569"
-         |
-         |         AWSCredentialsProvider = "com.weightwatchers.reactive.kinesis.common.TestCredentials|foo|bar"
-         |
-         |         regionName = us-east-1
-         |
-         |         workerId = "$workerId"
-         |
-         |         # Reduce default values, to speed up the integration test.
-         |         maxRecords = $maxRecords
-         |         metricsLevel = NONE
-         |         failoverTimeMillis = 1000
-         |         shardSyncIntervalMillis = 1000
-         |         idleTimeBetweenReadsInMillis = 200
-         |         parentShardPollIntervalMillis = 1000
-         |      }
-         |
-         |      worker {
-         |         batchTimeoutSeconds = 2
-         |         failedMessageRetries = 0
-         |         failureTolerancePercentage = 0
-         |         gracefulShutdownHook = false
-         |         shutdownTimeoutSeconds = 10
-         |      }
-         |   }
-         |}
+           |kinesis {
+           |
+           |   application-name = "$appName"
+           |
+           |   testProducer {
+           |      stream-name = "$streamName"
+           |
+           |      kpl {
+           |         Region = us-east-1
+           |
+           |         CloudwatchEndpoint = localhost
+           |         CloudwatchPort = 4582
+           |
+           |         KinesisEndpoint = localhost
+           |         KinesisPort = 4568
+           |
+           |         VerifyCertificate = false
+           |      }
+           |   }
+           |
+           |   testConsumer {
+           |      # The name of the consumer stream, MUST be specified per consumer
+           |      stream-name = "$streamName"
+           |
+           |      # Use localstack for integration test
+           |      kcl {
+           |         kinesisEndpoint = "https://localhost:4568"
+           |         dynamoDBEndpoint = "https://localhost:4569"
+           |
+           |         AWSCredentialsProvider = "com.weightwatchers.reactive.kinesis.common.TestCredentials|foo|bar"
+           |
+           |         regionName = us-east-1
+           |
+           |         workerId = "$workerId"
+           |
+           |         # Reduce default values, to speed up the integration test.
+           |         maxRecords = $maxRecords
+           |         metricsLevel = NONE
+           |         failoverTimeMillis = 1000
+           |         shardSyncIntervalMillis = 1000
+           |         idleTimeBetweenReadsInMillis = 200
+           |         parentShardPollIntervalMillis = 1000
+           |      }
+           |
+           |      worker {
+           |         batchTimeoutSeconds = 2
+           |         failedMessageRetries = 0
+           |         failureTolerancePercentage = 0
+           |         gracefulShutdownHook = false
+           |         shutdownTimeoutSeconds = 10
+           |      }
+           |   }
+           |}
       """.stripMargin
       )
       .getConfig("kinesis")
       .withFallback(defaultKinesisConfig)
 
   def consumerConfFor(conf: Config, consumer: String = "testConsumer"): ConsumerConf = {
-    val config     = ConsumerConf(conf, consumer)
+    val config = ConsumerConf(conf, consumer)
     val clientConf = config.kclConfiguration.getKinesisClientConfiguration
     // reduce the thread pool to a small size (default is 50)
     // this config option is missing via typesafe config and should be added.
@@ -91,20 +103,24 @@ trait KinesisConfiguration {
 
 /**
   * Mixin this trait to your test to interact with Kinesis.
-  * Every suite will have a clean Kinesis and Dynamo as well as one Stream with 2 Shards with 100 Messages each.
+  * Every suite will have a clean Kinesis and Dynamo as well as one Stream
+  * with `TestStreamNumberOfShards` Shards with `TestStreamNrOfMessagesPerShard` Messages each.
   *
   * Deletes only the table and stream for the CURRENT test (TestStreamName).
   *
   */
-trait KinesisKit
-    extends BeforeAndAfter
+trait KinesisSuite
+  extends BeforeAndAfter
     with BeforeAndAfterAll
     with StrictLogging
-    with KinesisConfiguration { self: Suite =>
+    with KinesisConfiguration {
+  self: Suite =>
 
-  val TestStreamName: String               = self.suiteName
+  val TestStreamName: String = self.suiteName
   val TestStreamNrOfMessagesPerShard: Long = 100
-  val TestStreamNumberOfShards: Long       = 2
+  val TestStreamNumberOfShards: Long = 2
+
+  private lazy val kclSetupConfig = consumerConfFor(kinesisConfig(streamName = TestStreamName, appName = suiteName)).kclConfiguration
 
   /**
     * Cleanup dynamo before each test
@@ -132,23 +148,48 @@ trait KinesisKit
     kinesisClient.shutdown()
   }
 
+  /**
+    * Wrap your code in this
+    *
+    * @param appName
+    */
+  class WithKinesis(val appName: String) {
+    val workerIdGen: Iterator[String] = 1.to(Int.MaxValue).iterator.map(id => s"wrk-$id")
+    def consumerConf(appName: String, batchSize: Long): ConsumerConf = {
+      consumerConfFor(
+        kinesisConfig(streamName = TestStreamName,
+          appName = appName,
+          //workerId = appName + "-" + workerIdGen.next(),
+          maxRecords = batchSize.toInt)
+      )
+    }
+
+    // proactively create the lease table for this application.
+    // KCL does not handle this reliably, which makes the test brittle.
+    createLeaseTable(appName)
+  }
+
   protected def createKinesisStream(): Unit = {
     kinesisClient.createStream(TestStreamName, TestStreamNumberOfShards.toInt)
 
     // Block until the stream is ready to rumble.
     while (kinesisClient
-             .describeStream(TestStreamName)
-             .getStreamDescription
-             .getStreamStatus != "ACTIVE") {
+      .describeStream(TestStreamName)
+      .getStreamDescription
+      .getStreamStatus != "ACTIVE") {
       Thread.sleep(100)
     }
     logger.info(s"Stream: $TestStreamName is created.")
   }
 
+  /**
+    * There seems to be a race condition in KCL when the lease table is created/accessed, which made the tests brittle.
+    * This creates the lease table proactively to remedy this shortcoming
+    */
   protected def createLeaseTable(applicationName: String): Unit = {
     val manager = new LeaseManager[KinesisClientLease](s"$applicationName-$TestStreamName",
-                                                       dynamoClient,
-                                                       new KinesisClientLeaseSerializer())
+      dynamoClient,
+      new KinesisClientLeaseSerializer())
     manager.createLeaseTableIfNotExists(1l, 1l)
     while (!manager.leaseTableExists()) Thread.sleep(100)
   }
@@ -174,26 +215,22 @@ trait KinesisKit
   }
 
   lazy val kinesisClient: AmazonKinesisAsync = {
-    val kcl = consumerConfFor(kinesisConfig(streamName = TestStreamName, appName = suiteName)).kclConfiguration
-
     AmazonKinesisAsyncClientBuilder
       .standard()
-      .withClientConfiguration(kcl.getKinesisClientConfiguration)
+      .withClientConfiguration(kclSetupConfig.getKinesisClientConfiguration)
       .withEndpointConfiguration(
-        new EndpointConfiguration(kcl.getKinesisEndpoint, kcl.getRegionName)
+        new EndpointConfiguration(kclSetupConfig.getKinesisEndpoint, kclSetupConfig.getRegionName)
       )
       .withCredentials(TestCredentials.Credentials)
       .build()
   }
 
   lazy val dynamoClient: AmazonDynamoDB = {
-    val kcl = consumerConfFor(kinesisConfig(streamName = TestStreamName, appName = suiteName)).kclConfiguration
-
     AmazonDynamoDBClientBuilder
       .standard()
-      .withClientConfiguration(kcl.getDynamoDBClientConfiguration.withMaxConnections(2))
+      .withClientConfiguration(kclSetupConfig.getDynamoDBClientConfiguration.withMaxConnections(2))
       .withEndpointConfiguration(
-        new EndpointConfiguration(kcl.getDynamoDBEndpoint, kcl.getRegionName)
+        new EndpointConfiguration(kclSetupConfig.getDynamoDBEndpoint, kclSetupConfig.getRegionName)
       )
       .withCredentials(TestCredentials.Credentials)
       .build()
